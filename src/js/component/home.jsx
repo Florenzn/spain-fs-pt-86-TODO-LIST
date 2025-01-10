8fd536af6e13479449d5f4d8cb3c61c8dcb75a31
Einar <einarescobarclavijo@gmail.com>
2025-01-10 15:10:51 +0000

    First Commit

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,71 +1,97 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 
 //include images into your bundle
 import rigoImage from "../../img/rigo-baby.jpg";
 
 //create your first component
-const lista = [{
-	numero: "ocho",
-	color: "negro",
-	pais: "Alemania",
-  
-  }, {
-	numero: "dos",
-	color: "blanco",
-	pais: "España",
-  
-  }, {
-	numero: "cinco",
-	color: "gris",
-	pais: "Camboya",
-  
-  }]
-  
-const Todo = () => {
-	const [state, setState] = useState(lista)
-	const [color, setColor] = useState("")
+const addTodo = async (label) =>{
+	const response = await fetch("https://playground.4geeks.com/todo/users/einar", {
+		method: "POST",
+		headers: {
+			"Content-Type": "appLication/json"
+		},
+		body: JSON.stringify({ is_done: false, label })
 
-    const añadir = () => {
-		setState([...state, {color: color}])
-		setColor("")
-	}
-	const eliminarItem = (listValorIndex) => {
-		const list = state.filter((listItem, index) => {
-			return index != listValorIndex
-		})
-		setState(list)
-	}
-
-	return (<>
-	<div className="conatainer">
-	<p className="header"> TODO LIST </p>
-	<input className="barra" value={color} onChange={(e) => {
-		setColor(e.target.value)
-	}} />
-	<button className="boton" onClick={añadir}>Añadir</button>
-	
-	<ul className="cuerpo">
-		{state.map((item, index) => {
-			return (<>
-			<li className="lista">
-				{item.color} <button className="botonX" onClick={() => eliminarItem(index)}>X</button>
-			</li>
-			</>)
-		})}
-		 
-	</ul>
-	<div className="contador" >
-        Total de elementos: {state.length}
-      </div>
-
-	</div>
-	
-
-	</>
 		
-	
 
-	)
+	});
+	const todos = await response.json();
+	return todos;
 }
+
+const deleteTodo = async (id) => {
+	await fetch( 'https://playground.4geeks.com/todo/users/lista/$(id)', {
+		method: "DELETE"
+	})
+}
+  
+
+const Todo = () => {
+  const [state, setState] = useState([]);
+  const [input, setInput] = useState("");
+
+  useEffect(() => {
+    actualizarLista();
+  }, []); 
+  const actualizarLista = async () => {
+    const listaTodo = await getTodos();
+    setState(listaTodo);
+  };
+
+  const añadir = async (label) => { 
+    await addTodo(label);
+    await actualizarLista();
+    setInput(""); 
+  };
+
+  const eliminarItem = async (id) => {
+    await deleteTodo(id);
+    await actualizarLista();
+  };
+
+  return (
+    <>
+      <div className="container">
+        <p className="header"> TODO LIST </p>
+        <input
+          className="barra"
+          value={input}
+          onChange={(e) => {
+            setInput(e.target.value);
+          }}
+        />
+        <button className="boton" onClick={() => añadir(input)}>
+          Añadir
+        </button>
+
+        <ul className="cuerpo">
+          {state.map((item) => {
+			return (
+				<>
+				<li  className="lista">
+			{item.label}{" "}
+			<button
+			  className="botonX"
+			  onClick={() => eliminarItem(item.id)}
+			>
+			  X
+			</button>
+		  </li>
+		  </>
+
+			);
+		})}
+			
+		  
+            
+
+        </ul>
+        <div className="contador">
+          Total de elementos: {state.length}
+        </div>
+      </div>
+    </>
+  );
+};
 
 export default Todo;
